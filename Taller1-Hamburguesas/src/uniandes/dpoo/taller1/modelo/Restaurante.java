bddb3494143d493eddd5a6ec64b96b864f9e626a
Nicolas-creador <74687268+Nicolas-creador@users.noreply.github.com>
2022-02-15 22:29:21 -0500

    Update Restaurante.java

--- Taller1-Hamburguesas/src/uniandes/dpoo/taller1/modelo/Restaurante.java
+++ Taller1-Hamburguesas/src/uniandes/dpoo/taller1/modelo/Restaurante.java
@@ -1,97 +1,156 @@
 package uniandes.dpoo.taller1.consola;
 
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import uniandes.dpoo.taller1.modelo.Pedido;
-public class Restaurante 
+import java.util.Scanner;
+
+
+
+public class Restaurante
 {
+	private ArrayList<Pedido> pedidos;
+	private int pedidoB; 
+	private HashMap<String, ProductoMenu> Menus;
+	private HashMap<String, Ingrediente> ingredientes;
+	private HashMap<String, Combo> combos;
+	
 	public Restaurante()
 	{
+		pedidos = new ArrayList<Pedido>();
+		Menus = new HashMap<String, ProductoMenu>();
+		ingredientes = new HashMap<String, Ingrediente>();
+		combos = new HashMap<String, Combo>();	
+	}
+	
+	
+	public void iniciarPedido(String nombreCliente, String direccionCliente)
+	{
+		boolean pedidoP = !haypedidoB();
+		
+		if (pedidoP)
+		{
+			pedidoB = 1;
+			Pedido elPedido = new Pedido(nombreCliente, direccionCliente);
+			pedidos.add(elPedido);	
+		}
+	}
+	
+	
+	public ArrayList<Pedido> consultarPedidos()
+	{
+		return pedidos;
+	}
+	
+	
+	public void cerrarYGuardarPedido()
+	{
+		pedidoB = 0;
+	}
+	
+	
+	public Pedido getpedidoB()
+	{
+		return pedidos.get(pedidos.size() - 1);
 		
 	}
 	
-	public void IniciarPedido(String nombreCliente,String direccionCliente)
+	
+	public ArrayList<Producto> getMenus()
 	{
-	    Pedido pedidoEnCurso = new Pedido(nombreCliente,direccionCliente);  
+		ArrayList<Producto> newList = new ArrayList<>(Menus.values());
+		return newList;
 	}
 	
-	public void cerrarYGuardarPedido(Pedido pedidoEnCurso)
+	
+	public ArrayList<Producto> getCombos()
 	{
-		pedidoEnCurso = new ArrayList<Pedido>(this.IniciarPedido(null, null));
+		ArrayList<Producto> newList = new ArrayList<>(combos.values());
+		return newList;
 	}
 	
-	public Pedido getPedidoEnCurso(Pedido pedidoEnCurso)
-	{
-		return pedidoEnCurso;
-	}
-
-	
-	public ArrayList<Producto> getMenuBase(Map menu) 
-	{
-		ArrayList menuBase = new ArrayList(); 
-		for (String clave: menu.keySet()) 
-		 {
-		    int valor = menu.get(clave);
-		    ProductoMenu producto = new ProductoMenu(clave,valor);
-		    menuBase.add(producto);
-		  }
-	}
 	
 	public ArrayList<Ingrediente> getIngredientes()
 	{
-		
+		ArrayList<Ingrediente> newList = new ArrayList<>(ingredientes.values());
+		return newList;
 	}
 	
-	public void cargarInformacionRestaurante()
+	
+	public void cargarInformacionRestaurante(File archivoIngredientes,
+											 File archivoMenu,
+											 File archivoCombos) throws FileNotFoundException
 	{
-	File archivoIngredientes = new File("/Taller1-Hamburguesas/__MACOSX/data/combos.txt"+"/Taller1-Hamburguesas/__MACOSX/data/ingredientes.txt"+"/Taller1-Hamburguesas/__MACOSX/data/menu.txt");
-	
-	BufferedReader menu = new BufferedReader(new FileReader(archivoIngredientes));
-
-	  String strng;
-	  while ((strng = menu.readLine()) != null)
-	    System.out.println(strng);	  	  
+		cargarIngredientes(archivoIngredientes);
+		cargarMenu(archivoMenu);
+		cargarCombos(archivoCombos);
 	}
 	
 	
-	private void cargarMenu()	
-	{ 
-		Map <String,Integer> menu = new HashMap< String,Integer>();		
-		BufferedReader br = new BufferedReader(new FileReader("menu.txt"));
-		String linea = br.readLine(); 
-		linea = br.readLine();
-		while (linea != null) 
-		{
-			// Separar los valores que estaban en una linea
-			String[] partes = linea.split(";");
+	private void cargarIngredientes(File archivoIngredientes) throws FileNotFoundException
+	{
+		Scanner Reader = new Scanner(archivoIngredientes);
+		while (Reader.hasNextLine()) {
+			String Line = Reader.nextLine();
+			String[] partes = Line.split(";");
+			String nombreIngrediente = partes[0];
+			int costoAdicional = Integer.parseInt(partes[1]);
+			
+			Ingrediente ingr = new Ingrediente(nombreIngrediente, costoAdicional);
+			ingredientes.put(nombreIngrediente, ingr);
+		}
+		Reader.close();
+	}
+	
+	
+	private void cargarMenu(File archivoMenu) throws FileNotFoundException
+	{
+		Scanner Reader = new Scanner(archivoMenu);
+		while (Reader.hasNextLine()) {
+			String Line = Reader.nextLine();
+			String[] partes = Line.split(";");
 			String nombreProducto = partes[0];
-			Integer precioBase = Integer.parseInt(partes[1]);
-		    
-			if ( menu.get(nombreProducto)== null )
-		    	{
-				 menu.put(nombreProducto,precioBase);
-		    	}
-					
-		 linea = br.readLine(); // Leer la siguiente linea
-	    }
+			int precio = Integer.parseInt(partes[1]);
+			
+			ProductoMenu Producto = new ProductoMenu(nombreProducto, precio);
+			Menus.put(nombreProducto, Producto);
+		}
+		Reader.close();
+	}
+	
+	
+	private void cargarCombos(File archivoCombos) throws FileNotFoundException
+	{
+		Scanner Reader = new Scanner(archivoCombos);
+		while (Reader.hasNextLine()) {
+			String Line = Reader.nextLine();
+			String[] partes = Line.split(";");
+			String nombreCombo = partes[0];
+			double descuento = Double.parseDouble(partes[1].replace("%", ""));
+			descuento = descuento/100;
+
+			Combo combo = new Combo(nombreCombo, descuento);
+			
+			for(int i=2; i<partes.length ; i++) {
+				String nombreProducto = partes[i];
+				ProductoMenu nProducto = Menus.get(nombreProducto);
+				combo.agregarItemACombo(nProducto);
+			}
+			combos.put(nombreCombo, combo);
+		}
+		Reader.close();
+	}
+	
+	
+	public boolean haypedidoB()
+	{
+		boolean a = false;
 		
-		br.close();    
-	 }
-	
-	
-	
+		if (pedidoB==1)
+			a = true;
+
+		return a;
+	}
 }
-
-
-	
-
-
-
