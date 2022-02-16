b2dd72192bcaee60a07207ad17b9fe7025ef6ae9
Nicolas-creador <74687268+Nicolas-creador@users.noreply.github.com>
2022-02-15 22:28:47 -0500

    Update ProductoAjustado.java

--- Taller1-Hamburguesas/src/uniandes/dpoo/taller1/procesamiento/ProductoAjustado.java
+++ Taller1-Hamburguesas/src/uniandes/dpoo/taller1/procesamiento/ProductoAjustado.java
@@ -1,30 +1,75 @@
 package uniandes.dpoo.taller1.consola;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 
 public class ProductoAjustado implements Producto
 {
 	private ProductoMenu base;
-	private ArrayList<Ingrediente>agregados;
-	private ArrayList<Ingrediente>eliminados;
+	private LinkedList<Ingrediente> agregados;
+	private LinkedList<Ingrediente> eliminados;
 	
-	public ProductoAjustado(ProductoMenu productoMenuBase)
+	public ProductoAjustado(ProductoMenu base)
 	{
-		base=productoMenuBase;
+		this.base = base;
+		agregados = new LinkedList<Ingrediente>();
+		eliminados = new LinkedList<Ingrediente>();
 	}
 
-	public int getPrecio() 
+	
+	public String getNombre()
 	{
-		return base.getPrecio();
+		String nombre = base.getNombre();
+		return nombre;
 	}
-
-	public String getNombre() 
+	
+	
+	public int getPrecio()
 	{
-		return base.getNombre();
+		
+		int precio = base.getPrecio();
+		Iterator<Ingrediente> iter_ag = agregados.iterator();
+		
+		while (iter_ag.hasNext())
+		{
+			Ingrediente i_agregado = iter_ag.next();
+			precio += i_agregado.getCostoAdicional();
+		}
+		
+		return precio;
 	}
-
-	public String generarTextoFactura() 
+	
+	
+	public String generarTextoFactura()
 	{
-		return base.generarTextoFactura();
+		String nombre = getNombre();
+		String precio_base = Integer.toString(base.getPrecio());
+		String precio_total = Integer.toString(getPrecio());
+		String texto = "Producto ajustado";
+		texto += "Producto Base: " + nombre + "  " + precio_base;
+		
+		
+		Iterator<Ingrediente> iter_ag = agregados.iterator();
+		Iterator<Ingrediente> iter_el = eliminados.iterator();
+		
+		while (iter_ag.hasNext())
+		{
+			Ingrediente i_agregado = iter_ag.next();
+			String ag_nombre = i_agregado.getNombre();
+			String adicional = Integer.toString(i_agregado.getCostoAdicional());
+			texto += "Extra de " + ag_nombre + " " + adicional;
+		}
+		
+		while (iter_el.hasNext())
+		{
+			Ingrediente i_eliminado = iter_el.next();
+			String el_nombre = i_eliminado.getNombre();
+			texto += "Sin " + el_nombre;
+		}
+		
+		texto += "Total " + nombre + " " + precio_total;
+		
+		return texto;
 	}
 }
